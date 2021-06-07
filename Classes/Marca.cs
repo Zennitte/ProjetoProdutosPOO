40a8d09ae90f1debe0415042c19c8be7d3999578
Kauê Camargo <camargokaue92@gmail.com>
2021-06-07 17:00:39 -0300

    Marca Parcialmente Concluida

--- Classes/Marca.cs
+++ Classes/Marca.cs
@@ -1,7 +1,50 @@
+using System;
+using System.Collections.Generic;
+using Projeto_ProdutosPOO.Interfaces;
+
 namespace Projeto_ProdutosPOO.Classes
 {
-    public class Marca
+    public class Marca : IMarca
     {
-        
+        private int Codigo { get; set; }
+
+        internal static void Cadastrar(object marca)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string NomeMarca { get; set; }
+        private DateTime DataCadastro { get; set; }    
+        List<Marca> marcas = new List<Marca>();
+        Marca novaMarca = new Marca();
+        public string Cadastrar(Marca marca)
+        {
+            Console.WriteLine("Qual o código da marca?");
+            novaMarca.Codigo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o nome da marca?");
+            novaMarca.NomeMarca = Console.ReadLine();
+            
+            novaMarca.DataCadastro = DateTime.Now;
+
+            marca = novaMarca;
+
+            marcas.Add(marca);
+            return "Marca Cadastrada";
+        }
+
+        public string Deletar(Marca marca)
+        {
+            Console.WriteLine("Qual marca você quer remover");
+            string marcaRemovida = Console.ReadLine();
+
+            marcas.RemoveAll(x => x.NomeMarca == marcaRemovida);
+            return "Marca Removida";
+        }
+
+        public List<Marca> Listar()
+        {
+            return marcas;
+        }
     }
 }
--- Interfaces/IMarca.cs
+++ Interfaces/IMarca.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using Projeto_ProdutosPOO.Classes;
+
 namespace Projeto_ProdutosPOO.Interfaces
 {
     public interface IMarca
     {
-         
-         
+         string Cadastrar(Marca marca);
+         List<Marca> Listar();
+         string Deletar(Marca marca);
     }
 }
