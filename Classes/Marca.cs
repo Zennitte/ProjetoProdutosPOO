6442c9079603924289543a52087f9339b151214b
Kauê Camargo <camargokaue92@gmail.com>
2021-06-08 14:38:10 -0300

    Branch Marca Finalizada

--- Classes/Marca.cs
+++ Classes/Marca.cs
@@ -16,29 +16,25 @@ namespace Projeto_ProdutosPOO.Classes
         private string NomeMarca { get; set; }
         private DateTime DataCadastro { get; set; }    
         List<Marca> marcas = new List<Marca>();
-        Marca novaMarca = new Marca();
+        // Marca novaMarca = new Marca();
+        public Marca()
+        {
+
+        }
+        public Marca(string _nomeMarca)
+        {
+            NomeMarca = _nomeMarca;
+            DataCadastro = DateTime.Now;            
+        }
         public string Cadastrar(Marca marca)
         {
-            Console.WriteLine("Qual o código da marca?");
-            novaMarca.Codigo = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Qual o nome da marca?");
-            novaMarca.NomeMarca = Console.ReadLine();
-            
-            novaMarca.DataCadastro = DateTime.Now;
-
-            marca = novaMarca;
-
             marcas.Add(marca);
             return "Marca Cadastrada";
         }
 
         public string Deletar(Marca marca)
         {
-            Console.WriteLine("Qual marca você quer remover");
-            string marcaRemovida = Console.ReadLine();
-
-            marcas.RemoveAll(x => x.NomeMarca == marcaRemovida);
+            marcas.RemoveAll(x => x.NomeMarca == marca.NomeMarca);
             return "Marca Removida";
         }
 
